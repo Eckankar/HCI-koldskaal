ce0dde4f74811dab817e53b8d5b5362d5986f6e4
Mathias Svensson <freaken@freaken.dk>
2010-09-09 14:25:54 +0200

    Lavede menuBars til mellemrum

--- koldstart/kode/index.php
+++ koldstart/kode/index.php
@@ -3,21 +3,21 @@ $_VIEW = array();
 $_VIEW["title"] = "bleh";
 $_VIEW["content"] = "<div>THIS IS PENSIIIOOOOOOOOOON!!!!!</div>";
 $_VIEW["level1Menus"] = array(
-  array("url" => "http://google.dk", "title" => "Google"),
-  array("url" => "http://yahoo.com", "title" => "Yahoo!"),
-  array("url" => "http://bing.com", "title" => "Bing"),
+  array("url" => "?level1=0", "title" => "Menu 1"),
+  array("url" => "?level1=1", "title" => "Menu 2"),
+  array("url" => "?level1=2", "title" => "Menu 3"),
 );
 
 $_VIEW["level2Menus"] = array(
-  array("url" => "http://google.dk", "title" => "Google"),
-  array("url" => "http://yahoo.com", "title" => "Yahoo!"),
-  array("url" => "http://bing.com", "title" => "Bing"),
+  array("url" => "?level1=" . $_GET["level1"] . "&level2=0", "title" => "Undermenu 1"),
+  array("url" => "?level1=" . $_GET["level1"] . "&level2=1", "title" => "Undermenu 2"),
+  array("url" => "?level1=" . $_GET["level1"] . "&level2=2", "title" => "Undermenu 3"),
 );
 
 $_VIEW["level3Menus"] = array(
-  array("url" => "http://google.dk", "title" => "Google"),
-  array("url" => "http://yahoo.com", "title" => "Yahoo!"),
-  array("url" => "http://bing.com", "title" => "Bing"),
+  array("url" => "?level1=" . $_GET["level1"] . "&level2=" . $_GET["level2"] . "&level3=0", "title" => "Under<sup>2</sup>menu 1"),
+  array("url" => "?level1=" . $_GET["level1"] . "&level2=" . $_GET["level2"] . "&level3=1", "title" => "Under<sup>2</sup>menu 2"),
+  array("url" => "?level1=" . $_GET["level1"] . "&level2=" . $_GET["level2"] . "&level3=2", "title" => "Under<sup>2</sup>menu 3"),
 );
 
 $_VIEW["level3Breadcrumb"] = array(
@@ -26,8 +26,8 @@ $_VIEW["level3Breadcrumb"] = array(
   array("url" => "http://bing.com", "title" => "Bing"),
 );
 
-$_VIEW["level1Id"] = 1;
-$_VIEW["level2Id"] = 2;
+$_VIEW["level1Id"] = isset($_GET["level1"]) ? $_GET["level1"] : 0;
+$_VIEW["level2Id"] = isset($_GET["level2"]) ? $_GET["level2"] : 0;
 
 include "site.php";
 ?>
