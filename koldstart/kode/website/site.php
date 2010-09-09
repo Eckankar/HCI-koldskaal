cdf2a92abaa1c12aec65688356f056d877f30cc4
Mathias Svensson <freaken@freaken.dk>
2010-09-09 15:14:14 +0200

    Flere bugfixes

--- koldstart/kode/website/site.php
+++ koldstart/kode/website/site.php
@@ -15,7 +15,7 @@
     <nav id="level1">
       <ul>
 <?php foreach($_VIEW["level1Menus"] as $id => $link): ?>
-        <li class="<?=($id == $_VIEW["level1Id"] ? "selected" : "unselected") . " lightMenu" . $id ?>"><a href="<?= urlencode($link["url"])?>"><?=$link["title"]?></a></li>
+        <li class="<?=($id == $_VIEW["level1Id"] ? "selected" : "unselected") . " lightMenu" . $id ?>"><a href="<?= htmlentities($link["url"])?>"><?=$link["title"]?></a></li>
 <?php endforeach ?>
       </ul>
     </nav>
@@ -23,7 +23,7 @@
     <nav id="level2">
       <ul>
 <?php foreach($_VIEW["level2Menus"] as $id => $link): ?>
-        <li class="<?=$id == $_VIEW["level2Id"] ? "selected darkMenu" . $_VIEW["level1Id"] : "unselected lightMenu" . $_VIEW["level1Id"] ?>"><a href="<?= urlencode($link["url"])?>"><?=$link["title"]?></a></li>
+        <li class="<?=$id == $_VIEW["level2Id"] ? "selected darkMenu" . $_VIEW["level1Id"] : "unselected lightMenu" . $_VIEW["level1Id"] ?>"><a href="<?= htmlentities($link["url"])?>"><?=$link["title"]?></a></li>
 <?php endforeach ?>
       </ul>
     </nav>
@@ -31,11 +31,11 @@
     <aside>
       <ul id="level3" class="darkMenu<?= $_VIEW["level1Id"]?>">
 <?php foreach($_VIEW["level3Breadcrumb"] as $id => $link): ?>
-        <?=str_repeat("  ", $id)?><li><a href="<?= urlencode($link["url"])?>"><?=$link["title"]?></a></li>
+        <?=str_repeat("  ", $id)?><li><a href="<?= htmlentities($link["url"])?>"><?=$link["title"]?></a></li>
         <?=str_repeat("  ", $id)?><ul>
 <?php endforeach ?>
 <?php foreach($_VIEW["level3Menus"] as $id => $link): ?>
-        <?=str_repeat("  ", count($_VIEW["level3Breadcrumb"]))?><li><a href="<?= urlencode($link["url"])?>"><?=$link["title"]?></a></li>
+        <?=str_repeat("  ", count($_VIEW["level3Breadcrumb"]))?><li><a href="<?= htmlentities($link["url"])?>"><?=$link["title"]?></a></li>
 <?php endforeach ?>
 <?php for($n = count($_VIEW["level3Breadcrumb"])-1; $n >= 0; $n--): ?>
         <?=str_repeat("  ", $n)?></ul>
