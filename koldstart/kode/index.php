e2591b03fd265ed922794176fc09af8a66637256
Mathias Svensson <freaken@freaken.dk>
2010-09-09 14:13:35 +0200

    Generer nu dynamiske menuer

--- koldstart/kode/index.php
+++ koldstart/kode/index.php
@@ -2,5 +2,32 @@
 $_VIEW = array();
 $_VIEW["title"] = "bleh";
 $_VIEW["content"] = "<div>THIS IS PENSIIIOOOOOOOOOON!!!!!</div>";
+$_VIEW["level1Menus"] = array(
+  array("url" => "http://google.dk", "title" => "Google"),
+  array("url" => "http://yahoo.com", "title" => "Yahoo!"),
+  array("url" => "http://bing.com", "title" => "Bing"),
+);
+
+$_VIEW["level2Menus"] = array(
+  array("url" => "http://google.dk", "title" => "Google"),
+  array("url" => "http://yahoo.com", "title" => "Yahoo!"),
+  array("url" => "http://bing.com", "title" => "Bing"),
+);
+
+$_VIEW["level3Menus"] = array(
+  array("url" => "http://google.dk", "title" => "Google"),
+  array("url" => "http://yahoo.com", "title" => "Yahoo!"),
+  array("url" => "http://bing.com", "title" => "Bing"),
+);
+
+$_VIEW["level3Breadcrumb"] = array(
+  array("url" => "http://google.dk", "title" => "Google"),
+  array("url" => "http://yahoo.com", "title" => "Yahoo!"),
+  array("url" => "http://bing.com", "title" => "Bing"),
+);
+
+$_VIEW["level1Id"] = 1;
+$_VIEW["level2Id"] = 2;
+
 include "site.php";
 ?>
