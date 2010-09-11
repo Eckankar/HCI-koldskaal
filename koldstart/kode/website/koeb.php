1e63daaa0d6fa7a06f8c7042045b58744bbfe5bb
Sebastian Paaske Tørholm <Eckankar+github@gmail.com>
2010-09-11 18:36:18 +0200

    Købssiden done.

--- koldstart/kode/website/koeb.php
+++ koldstart/kode/website/koeb.php
@@ -56,7 +56,7 @@
 }
 #step_3{
   text-align:center;
-  background-color: <?= $step > 2 ? '#6666CC' : '#CCCCFF' ?>;
+  background-color: <?= $step == 3 ? '#6666CC' : ($step == 4 ? '#CCC' : '#CCCCFF') ?>;
   color:#fff;
 }
 #step_3 p{
@@ -65,16 +65,27 @@
   padding-bottom:2px;
   padding-left:0;
 }
+#step_4 p{
+  padding-top:2px;
+  padding-right:0;
+  padding-bottom:2px;
+  padding-left:0;
+}
+<?php
+    if ($step == 4) {
+        print "#step_4 { background-color: #6666CC }\n";
+    }
+?>
 #arrow_1{
   background: #ccccff url(https://s.yimg.com/lq/i/reg/ar_progress_sprite.png) <?= $step == 1 ? '-2333px' : ($step == 2 ? '-3890px' : '-1555px') ?> 50%;
   width:30px;
 }
 #arrow_2{
-  background: #ccccff url(https://s.yimg.com/lq/i/reg/ar_progress_sprite.png) <?= $step == 1 ? '-3110px' : ($step == 2 ? '-2333px' : '-3890px')  ?> 50%;
+  background: #ccccff url(https://s.yimg.com/lq/i/reg/ar_progress_sprite.png) <?= $step == 1 ? '-3110px' : ($step == 2 ? '-2333px' : ($step == 3 ? '-3890px' : '-1555px'))  ?> 50%;
   width:30px;
 }
 #arrow_3{
-  background: #ccccff url(https://s.yimg.com/lq/i/reg/ar_progress_sprite.png) <?= $step == 3 ? '-35px' : '-795px' ?> 50%;
+  background: #ccccff url(https://s.yimg.com/lq/i/reg/ar_progress_sprite.png) <?= $step == 3 ? '-35px' : ($step == 4 ? '-3900px' : '-795px') ?> 50%;
   width:20px;
 }
 .orderImage {
@@ -96,19 +107,31 @@
     list-style: none;
     margin-bottom: 2em;
 }
+
+#insideOrderInfo {
+            padding: 0.5em;
+}
+
+input[type="radio"] {
+    margin-right: .5em;
+}
+
+.addrCont {
+    margin-bottom: 1em;
+}
 </style>
 <!-- end of progress bar stuff -->
 <header id="innerOrderHeader">
 <h1>Køb af billetter til Violinkoncert (Brahms)</h1>
 <table width="100%" cellpadding="0" cellspacing="0" border="0" id="progressBar">
   <tbody><tr valign="center">
-    <td id="step_0"><p>Forløb</p></td>
     <td id="steps">
       <table width="100%" cellpadding="0" cellspacing="0" border="0">
         <tbody><tr valign="center">
 	  <td id="step_1"><p>Vælg antal</p></td><td id="arrow_1">&nbsp;</td>
 	  <td id="step_2"><p>Indtast oplysninger</p></td><td id="arrow_2">&nbsp;</td>
-	  <td id="step_3"><p>Ordre fuldført</p></td><td id="arrow_3">&nbsp;</td>
+	  <td id="step_3"><p>Vælg betalingsform</p></td><td id="arrow_3">&nbsp;</td>
+	  <td id="step_4"><p>Ordre fuldført</p></td><td id="arrow_4">&nbsp;</td>
         </tr>
       </tbody></table>
   </td></tr>
@@ -138,10 +161,121 @@
         <div style="clear: both"></div>
     </div>
 <? elseif ($step == 2): ?>
+    <style>
+        #insideOrderInfo {
+            padding: 0.5em;
+        }
+    </style>
     <div id="orderInfo">
-        
-        <div style="clear: both"></div>
+        <div id="insideOrderInfo">
+            <form style="display: inline">
+            <div class="addrCont">
+                <input type="radio" name="addressChoice" checked/><label>Bestil med din standardadresse:</label>
+                <address>
+                    Karl Friedrich Codierer <br/>
+                    Universitetsparken 1 <br />
+                    2100 København Ø <br />
+                    Tlf: +45 35321435 <br />
+                    Email: <a href="mailto:spiltirsdag@diku.dk">spiltirsdag@diku.dk</a>
+                </address>
+            </div>
+            <div class="addrCont">
+                <input type="radio"  name="addressChoice"/><label>Bestil med følgende adresse:</label>
+                <div class="cmxform">
+                    <fieldset style="border: none">
+                      <ol>
+                        <li>
+                          <label for="navn">Navn<em>*</em></label>
+                          <input type="text" id="navn" name="navn" />
+                        </li>
+                        <li>
+                          <label for="address">Addresse<em>*</em></label>
+                          <input type="text" name="address" id="address" />
+                        </li>
+                        <li>
+                          <label for="town-city">By<em>*</em></label>
+                          <input type="input" name="town-city" id="town-city" />
+                        </li>
+                        <li>
+                          <label for="postnummer">Postnummer<em>*</em></label>
+                          <input type="input" name="postnummer" id="postnummer" maxlength="4" />
+                        </li>
+                        <li>
+                          <label for="telefon">Telefon<em>*</em></label>
+                          <input type="input" name="telefon" id="telefon" />
+                        </li>
+                        <li>
+                          <label for="telefon">Email<em>*</em></label>
+                          <input type="input" name="name" id="email" />
+                        </li>
+                      </ol>
+                    </fieldset>
+                </div>
+            </div></form>
+            
+            <form style="display: inline">
+            <?
+                $_GET['koeb_step'] = 3;
+                foreach ($_GET as $k => $v) {
+                    print "<input type='hidden' name='$k' value='$v' />";
+                }
+            ?>
+            <input type="submit" value="Fortsæt" />
+            </form>
+            <div style="clear: both"></div>
+        </div>    
     </div>
 <? elseif ($step == 3): ?>
+<div id="orderInfo">
+    <div id="insideOrderInfo">
+        <form class="cmxform">
+            <fieldset>
+            <legend>Vælg betalingsform</legend>
+            <ul>
+                <li>
+                    <input type="radio" name="betalingsform" checked/> Betal med kreditkort
+                    <div>
+                        <label for="kortnr">Kortnummer</label>
+                        <input type="input" name="kortnr" id="kortnr" /><br/>
+                        <label for="sikkerhed">Sikkerhedskode</label>
+                        <input type="input" name="sikkerhed" id="sikkerhed" /><br/>
+                        <label for="udlob">Udløbsdato</label>
+                        <input type="input" name="udlob" id="udlob" />
 
+                    </div>
+                </li>
+                <li>
+                    <input type="radio" name="betalingsform" /> Betal med bankoverførsel
+                    <div>
+                        Hvis dette vælges, betal da 1080,00 kr. til følgende konto:<br/>
+                        <div>
+                            Reg nr.: 4929 <br/>
+                            Konto: 6621578323
+                        </div>
+                        Skriv teksten <strong style="padding-left: .5em; padding-right: .5em; border: 1px solid gray; background: silver">P5210CR</strong> i kommentarfeltet på betalingen, så vi ved at den er tilknyttet denne ordre.
+                    </div>
+                </li>
+            </ul>
+            </fieldset>
+        </form>
+        <form style="display: inline">
+            <?
+            $_GET['koeb_step'] = 4;
+            foreach ($_GET as $k => $v) {
+            print "<input type='hidden' name='$k' value='$v' />";
+            }
+            ?>
+            <input type="submit" value="Fortsæt" />
+        </form>
+        <div style="clear: both"></div>
+    </div>
+</div>
+<? elseif ($step == 4): ?>
+<div id="orderInfo" style="min-height: 100px">
+    <div id="insideOrderInfo">
+    Din ordre er hermed modtaget.
+    
+    Ordren vil blive behandlet inden for 5 arbejdsdage.
+    </div>
+</div>
 <? endif; ?>
