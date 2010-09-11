fdbe11785af0a5a0080b8ee6f8a42bfb6dbaf48c
Daniel Egeberg <daniel.egeberg@gmail.com>
2010-09-11 14:52:46 +0200

    hej

--- koldstart/kode/website/koncerter.php
+++ koldstart/kode/website/koncerter.php
@@ -4,13 +4,13 @@
 </header>
 
 <div id="kategoriVisning">
-    <div>
-        <img src="images/violinkoncert.jpg" title="Violinkoncert"/>
+    <div class="kategoriBox">
+        <img src="images/underkategori_violin.jpg" title="Violinkoncert"/>
         <h2><a href="#">Violinkoncert</a></h2>
         Totalt nice violinkoncert med masser af <acronym title="Laughing Out Loud">LOL</acronym>.
     </div>
-    <div>
-        <img src="images/liedkoncert.jpg" title="Liedkoncert"/>
+    <div class="kategoriBox">
+        <img src="images/underkategori_lied.jpg" title="Liedkoncert"/>
         <h2><a href="#">Liedkoncert</a></h2>
         Denne koncert og sikkert også nice.
     </div>
