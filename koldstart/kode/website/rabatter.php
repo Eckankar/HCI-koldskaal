1045eb2a8dd067c2a0d36f4422697c9b7ba61dde
Sebastian Paaske Tørholm <Eckankar+github@gmail.com>
2010-09-11 13:15:43 +0200

    Første udkast til rabatter.php

---
+++ koldstart/kode/website/rabatter.php
@@ -0,0 +1,42 @@
+<header>
+    <h1>Rabatter og tilbud</h1>
+    <p>Tilbud, rabatter og fordele fra vores mange samarbejdspartnere. Find de nyeste tilbud, oplevelser og produkter i kategorierne.</p>
+</header>
+
+<div id="kategoriVisning">
+    <div class="kategoriBox">
+        <img src="images/kategori_bekvemmeligheder.jpg" title="Bekvemmeligheder" alt="Bekvemmeligheder"/>
+        <h2>Bekvemmeligheder</h2>
+        Komfort til hjemmet
+    </div>
+    <div class="kategoriBox">
+        <img src="images/kategori_hjaelpemidler.jpg" title="Hjælpemidler" alt="Hjælpemidler"/>
+        <h2>Hjælpemidler</h2>
+        Tryghed og hjælp i dagligdagen
+    </div>
+    <div class="kategoriBox">
+        <img src="images/kategori_kommunikation.jpg" title="Kommunikation" alt="Kommunikation"/>
+        <h2>Kommunikation</h2>
+        Kontakt til omverdenen
+    </div>
+    <div class="kategoriBox">
+        <img src="images/kategori_koncert.jpg" title="Koncerter" alt="Koncerter"/>
+        <h2>Koncerter</h2>
+        Musik og oplevelser til sjælen
+    </div>
+    <div class="kategoriBox">
+        <img src="images/kategori_rejser.jpg" title="Rejser" alt="Rejser"/>
+        <h2>Rejser</h2>
+        Udforsk verden
+    </div>
+    <div class="kategoriBox">
+        <img src="images/kategori_transport.jpg" title="Transport" alt="Transport"/>
+        <h2>Transport</h2>
+        Kom omkring i dagligdagen
+    </div>
+    <div class="kategoriBox">
+        <img src="images/kategori_underholdning.jpg" title="Underholdning" alt="Underholdning"/>
+        <h2>Underholdning</h2>
+        Underholdning, museer og cirkus
+    </div>
+</div>
