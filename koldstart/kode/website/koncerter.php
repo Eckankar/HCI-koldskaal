faec117dbf801ac33ec7417a6d4a57011a1e71d7
Sebastian Paaske Tørholm <Eckankar+github@gmail.com>
2010-09-13 14:09:19 +0200

    Lidt bedre tekst i kategorierne.

--- koldstart/kode/website/koncerter.php
+++ koldstart/kode/website/koncerter.php
@@ -1,17 +1,17 @@
 <header>
     <h1>Koncerter</h1>
-    <p>Vi har masser af tilbud koncerter der vil give dig masser <acronym title="Laughing Out Loud">LOL</acronym>!</p>
+    <p>Musik og oplevelser til sjælen.</p>
 </header>
 
 <div id="kategoriVisning">
     <div class="kategoriBox">
         <a href="?level1=0&amp;level2=2&amp;level3=0&amp;violin=1"><img src="images/underkategori_violin.jpg" title="Violinkoncert"/>
         <h2>Violinkoncert</h2></a>
-        Totalt nice violinkoncert med masser af <acronym title="Laughing Out Loud">LOL</acronym>.
+        Nyd en afslappende violinkoncert
     </div>
     <div class="kategoriBox">
         <a href="javascript:alert('not implemented')"><img src="images/underkategori_lied.jpg" title="Liedkoncert"/>
         <h2>Liedkoncert</h2></a>
-        Denne koncert og sikkert også nice.
+        Et indslag fra liedens univers
     </div>
 </div>
